a5457193641f88c2031df54eafce0ec0fbedd2b0
Mateus Azevedo <mateuscorreiaazevedo@gmail.com>
2025-08-04 19:55:22 -0300

    chore(main): add Husky, Biome, and commitlint configuration with code quality tools

--- App.tsx
+++ App.tsx
@@ -13,7 +13,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#3498d8',
     alignItems: 'center',
     justifyContent: 'center',
   },
