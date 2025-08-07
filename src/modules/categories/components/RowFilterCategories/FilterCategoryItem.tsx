f16175bdd32a8f8a5b702e11c04c27dc93e65b2a
Mateus Azevedo <mateuscorreiaazevedo@gmail.com>
2025-08-06 22:31:54 -0300

    feat(main): implement category selection with active state and shadow styling

--- src/modules/categories/components/RowFilterCategories/FilterCategoryItem.tsx
+++ src/modules/categories/components/RowFilterCategories/FilterCategoryItem.tsx
@@ -1,16 +1,49 @@
-import { TouchableOpacity, View } from 'react-native';
+import clsx from 'clsx';
+import { Platform, StyleSheet, TouchableOpacity, View } from 'react-native';
+import { colors } from '../../../../assets/styles/colors';
 import { Typography } from '../../../core';
 import type { CategoryModel } from '../../models/Category';
 
-export function FilterCategoryItem(props: CategoryModel) {
+type FilterCategoryItemProps = CategoryModel & {
+  isActive?: boolean;
+  onPress?: (categoryId: string) => void;
+};
+
+const isAndroid = Platform.OS === 'android';
+
+export function FilterCategoryItem(props: FilterCategoryItemProps) {
+  const { _id, icon, name, isActive = false, onPress = () => {} } = props;
+
   return (
-    <TouchableOpacity className="items-center gap-2 px-2 py-4">
-      <View className="size-[52px] items-center justify-center rounded-full bg-white">
-        <Typography className="text-2xl">{props.icon}</Typography>
+    <TouchableOpacity
+      activeOpacity={0.7}
+      className={clsx(
+        'items-center gap-2 px-2 py-4',
+        isActive ? 'opacity-100' : 'opacity-40'
+      )}
+      onPress={() => onPress(_id)}
+    >
+      <View
+        className="size-[52px] items-center justify-center rounded-full bg-white"
+        style={style.rounded}
+      >
+        <Typography className="text-2xl">{icon}</Typography>
       </View>
       <Typography className="text-sm" weigth={600}>
-        {props.name}
+        {name}
       </Typography>
     </TouchableOpacity>
   );
 }
+
+const style = StyleSheet.create({
+  rounded: {
+    shadowColor: colors.gray500,
+    shadowOffset: {
+      height: 2,
+      width: 0,
+    },
+    shadowOpacity: isAndroid ? 1 : 0.15,
+    shadowRadius: 1,
+  },
+});
--- src/modules/categories/components/RowFilterCategories/index.tsx
+++ src/modules/categories/components/RowFilterCategories/index.tsx
@@ -1,8 +1,23 @@
+import { useCallback, useState } from 'react';
 import { FlatList, View } from 'react-native';
 import { categories } from '../../../../../mocks/categories';
 import { FilterCategoryItem } from './FilterCategoryItem';
 
 export function RowFilterCategories() {
+  const [selectedCategory, setSelectedCategory] = useState<string | null>(null);
+
+  const handleSelectCategory = useCallback((categoryId: string) => {
+    setSelectedCategory(prev => {
+      const newPrev = prev;
+
+      if (newPrev === categoryId) {
+        return null;
+      }
+
+      return categoryId;
+    });
+  }, []);
+
   return (
     <View className="mt-8 min-h-[74px]">
       <FlatList
@@ -10,7 +25,13 @@ export function RowFilterCategories() {
         data={categories}
         horizontal
         keyExtractor={(item, index) => `${item._id}-${index}`}
-        renderItem={({ item }) => <FilterCategoryItem {...item} />}
+        renderItem={({ item }) => (
+          <FilterCategoryItem
+            {...item}
+            isActive={selectedCategory === item._id}
+            onPress={handleSelectCategory}
+          />
+        )}
         showsHorizontalScrollIndicator={false}
       />
     </View>
