2b7cd2b45274a216992f5653cab74290cb019d74
Mateus Azevedo <mateuscorreiaazevedo@gmail.com>
2025-08-08 00:29:26 -0300

    refactor(main): remove empty state component from ingredients list

--- src/modules/products/components/ProductModal/IngredientsList/index.tsx
+++ src/modules/products/components/ProductModal/IngredientsList/index.tsx
@@ -1,6 +1,5 @@
 import { FlatList, View } from 'react-native';
 import type { Ingredient } from '../../../models/Ingredient';
-import { ProductModalIngredientsListEmptyState } from './EmptyState';
 import { ProductModalIngredientsListHeader } from './Header';
 import { IngredientItem } from './IngredientItem';
 
@@ -18,7 +17,6 @@ export function ProductModalIngredientsList({
         contentContainerClassName="gap-1"
         data={ingredients}
         keyExtractor={item => item._id}
-        ListEmptyComponent={ProductModalIngredientsListEmptyState}
         renderItem={({ item }) => <IngredientItem {...item} />}
         showsVerticalScrollIndicator={false}
       />
