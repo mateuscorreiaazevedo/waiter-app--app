fe6e89c97bb0988e4a15be7d5bab28c29f7fa315
Mateus Azevedo <mateuscorreiaazevedo@gmail.com>
2025-08-09 15:09:45 -0300

    feat(main): add loading state to category filter list

--- src/modules/categories/components/RowFilterCategories/index.tsx
+++ src/modules/categories/components/RowFilterCategories/index.tsx
@@ -1,12 +1,13 @@
 import { useCallback, useState } from 'react';
-import { FlatList, View } from 'react-native';
+import { ActivityIndicator, FlatList, View } from 'react-native';
+import { colors } from '../../../../assets/styles/colors';
 import { useFetchCategories } from '../../hooks/useFetchCategories';
 import { FilterCategoryItem } from './FilterCategoryItem';
 
 export function RowFilterCategories() {
   const [selectedCategory, setSelectedCategory] = useState<string | null>(null);
 
-  const { categories } = useFetchCategories();
+  const { categories, isLoading, isFetched } = useFetchCategories();
 
   const handleSelectCategory = useCallback((categoryId: string) => {
     setSelectedCategory(prev => {
@@ -22,20 +23,28 @@ export function RowFilterCategories() {
 
   return (
     <View className="min-h-[74px]">
-      <FlatList
-        contentContainerClassName="px-4"
-        data={categories}
-        horizontal
-        keyExtractor={(item, index) => `${item._id}-${index}`}
-        renderItem={({ item }) => (
-          <FilterCategoryItem
-            {...item}
-            isActive={selectedCategory === item._id}
-            onPress={handleSelectCategory}
-          />
-        )}
-        showsHorizontalScrollIndicator={false}
-      />
+      {isLoading && (
+        <View className="items-center justify-center">
+          <ActivityIndicator color={colors.primary} />
+        </View>
+      )}
+
+      {isFetched && (
+        <FlatList
+          contentContainerClassName="px-4"
+          data={categories}
+          horizontal
+          keyExtractor={(item, index) => `${item._id}-${index}`}
+          renderItem={({ item }) => (
+            <FilterCategoryItem
+              {...item}
+              isActive={selectedCategory === item._id}
+              onPress={handleSelectCategory}
+            />
+          )}
+          showsHorizontalScrollIndicator={false}
+        />
+      )}
     </View>
   );
 }
